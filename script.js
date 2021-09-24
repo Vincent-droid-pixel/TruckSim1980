3488fa477cc8f169571bc38ff554139a87fa0bd0
Vinciento <74552955+Vincent-droid-pixel@users.noreply.github.com>
2021-09-24 08:54:40 +0000

    bewegende weg en background en gameover

--- script.js
+++ script.js
@@ -2,59 +2,58 @@ function setup() {
 	createCanvas(500, 400);
 }
 
-var [xpos, ypos, xspeed, yspeed] = [225, 225, 0, 0];
+function preload(){
+  img1 =loadImage("Images/truck.png");
+  img2 =loadImage("Images/explosion.png")
+}
+
+ 
+
+
+var [xpos, ypos, zpos, xspeed, yspeed] = [225, 225, 255, 0, 0];
 
 function draw() {
-	background(225);
+	background(209,173,56);
 	
-	fill(0);
-	text("Use the arrow keys (or WASD) to move the square around", 25, 25);
-	
-	fill(0, 255, 0);
-	rect(xpos, ypos, 50, 50);
-	
-	if(xpos >= 0 && xpos + 50 <= 500) xpos += xspeed;
-	if(ypos >= 0 && ypos + 50 <= 500) ypos += yspeed;
+  fill(204);
+  triangle(zpos, 18, xpos+500, 360, xpos, 360);
+
+  fill(204);
+  triangle(zpos, 18, xpos+250, 360, xpos, 360);
+
+  fill(204);
+  triangle(zpos, 18, xpos-500, 360, xpos, 360);
+
+  fill(204);
+  triangle(zpos, 18, xpos-250, 360, xpos, 360);
+
+
+  image(img1,0,0,500,400);
+
+	textSize(32);
+	if(xpos - 50 >= -350 && xpos + 50 <= 850) {
+  xpos+= xspeed; 
+  }
+  else {
+    text('Game Over', 10, 30);
+    image(img2,0,0,500,400);
+  }
 }
 
 function keyPressed() {
-	switch(keyCode) {
-		case 37:
-		case 65:
-			xspeed = -2;
-			break;
-		case 39:
-		case 68:
-			xspeed = 2;
-			break;
-		case 38:
-		case 87:
-			yspeed = -2;
-			break;
-		case 40:
-		case 83:
-			yspeed = 2;
-			break;
-	}
+  if (keyCode === LEFT_ARROW) {
+    xspeed = 5;
+    } 
+  else if (keyCode === RIGHT_ARROW) {
+    xspeed = -5;
+    }
 }
 
 function keyReleased() {
-	switch(keyCode) {
-		case 37:
-		case 65:
-			xspeed = 0;
-			break;
-		case 39:
-		case 68:
-			xspeed = 0;
-			break;
-		case 38:
-		case 87:
-			yspeed = 0;
-			break;
-		case 40:
-		case 83:
-			yspeed = 0;
-			break;
-	}
+	if (keyCode === LEFT_ARROW) {
+    xspeed = 0;
+    } 
+  else if (keyCode === RIGHT_ARROW) {
+    xspeed = 0;
+    }
 }
