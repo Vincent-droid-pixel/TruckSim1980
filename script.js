7ccd65af228f8fc0db7eac392f0955f613e29cd7
Vinciento <74552955+Vincent-droid-pixel@users.noreply.github.com>
2021-09-25 13:51:54 +0000

    Startscherm gemaakt

--- script.js
+++ script.js
@@ -1,63 +1,98 @@
 function setup() {
 	createCanvas(500, 400);
 }
+let myFont;
 
 function preload(){
   img1 =loadImage("Images/Vrachtwagen.png");
   img2 =loadImage("Images/explosion.png");
   img3 =loadImage("Images/tegenligger1.png");
+  img4 =loadImage("Images/startscreen.png") 
+  myFont = loadFont('Fonts/StickNoBills-Regular.ttf');
 }
 
- 
 
-
+var i = 0
+var screen = 0;
 var [xpos, ypos, zpos, xspeed, yspeed] = [225, 225, 255, 0, 0];
 
 function draw() {
-	background(209,173,56);
-	
-  fill(69, 69, 69);
-  stroke(255, 255, 255);
-  strokeWeight(2);
-  triangle(zpos, 18, xpos+600, 400, xpos, 400);
+  if (screen == 0){
+    i = i + 1
+    textSize(30);
+    textFont(myFont);
+    fill('white')
+    if (i % 40 === 0){
+      image(img4,0,0,500,400);
+    } else if (i % 20 ===0){
+      text('PRESS ENTER TO START',130,200)
+    }
+  }else if (screen == 1){
+    background(209,173,56);
 
-  fill(69, 69, 69)
-  stroke(255, 255, 255);
-  strokeWeight(2);
-  triangle(zpos, 18, xpos+300, 400, xpos, 400);
+    fill(69, 69, 69)
+    stroke(255, 255, 255);
+    strokeWeight(2);
+    triangle(zpos, 18, xpos+300, 400, xpos, 400);
 
-  fill(69, 69, 69);
-  stroke(255, 255, 255);
-  strokeWeight(2);
-  triangle(zpos, 18, xpos-600, 400, xpos, 400);
+    fill(69, 69, 69);
+    stroke(255, 255, 255);
+    strokeWeight(2);
+    //setLineDash([5, 10]);
+    triangle(zpos, 18, xpos+600, 400, xpos+300, 400);
 
-  fill(69, 69, 69);
-  stroke(255, 255, 255);
-  strokeWeight(2);
-  triangle(zpos, 18, xpos-300, 400, xpos, 400);
+    fill(69, 69, 69);
+    stroke(255, 255, 255);
+    strokeWeight(2);
+    triangle(zpos, 18, xpos-300, 400, xpos, 400);
 
-  image(img3,250,50,50,50);
+    fill(69, 69, 69);
+    stroke(255, 255, 255);
+    strokeWeight(2);
+    //setLineDash([5, 10]);
+    triangle(zpos, 18, xpos-600, 400, xpos-300, 400);
 
-  image(img1,0,0,500,400);
+    image(img3,250,50,50,50);
 
-	textSize(75);
-	if(xpos - 50 >= -350 && xpos + 50 <= 850) {
-  xpos+= xspeed; 
-  }
-  else {
-    image(img2,0,0,500,400);
-    text('Game Over', 70, 70,); 
+    image(img1,0,0,500,400);
+
+    textSize(75);
+    if(xpos - 50 >= -350 && xpos + 50 <= 850) {
+    xpos+= xspeed; 
+    }
+    else {
+      image(img2,0,0,500,400);
+      text('Game Over', 70, 70,); 
+    }
   }
 }
+
+//function mousePressed() {
+//	if (screen == 0) {
+//		screen = 1}
+//}
 
 function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-    xspeed = 5;
+  if (screen == 0 && keyCode === ENTER){
+    screen = 1
+  }
+	else if (keyCode === LEFT_ARROW) {
+		 xspeed = 5;
     } 
-  else if (keyCode === RIGHT_ARROW) {
-    xspeed = - 5;
-    }
+  else if (keyCode === RIGHT_ARROW){
+    xspeed = -5
+  }
 }
+
+
+//function keyPressed() {
+//  if (keyCode === LEFT_ARROW) {
+//    xspeed = 5;
+//    } 
+//  else if (keyCode === RIGHT_ARROW) {
+//    xspeed = - 5;
+//    }
+//}
 
 function keyReleased() {
 	if (keyCode === LEFT_ARROW) {
@@ -66,4 +101,8 @@ function keyReleased() {
   else if (keyCode === RIGHT_ARROW) {
     xspeed = 0;
     }
+}
+
+function setLineDash(list) {
+  drawingContext.setLineDash(list);
 }
