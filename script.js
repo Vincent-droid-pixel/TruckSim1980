b72b81e9dda5e16e78f35446ee82bd72876e1e46
Tim Logtenberg <45438244+lgg-maartens@users.noreply.github.com>
2021-10-11 14:44:30 +0000

    LGG did the OO-shuffle

--- script.js
+++ script.js
@@ -6,10 +6,26 @@ function setup() {
 let myFont;
 let music;
 
+function getLane(lane, apos) {
+
+  if (lane == 1) {
+    return createVector(apos - 60, 800)
+  }
+  if (lane == 2) {
+    return createVector(apos + 500, 800)
+  }
+  if (lane == 3) {
+    return createVector(apos - 600, 800)
+  }
+  if (lane == 4) {
+    return createVector(apos + 1000, 800)
+  }
+}
+
 function preload() {
   img1 = loadImage("Images/truck.png");
   img2 = loadImage("Images/explosion.png");
-  img3 = loadImage("Images/tegenligger1.png");
+  tegenliggerImg = loadImage("Images/tegenligger1.png");
   img4 = loadImage("Images/startscreen.png");
   img9 = loadImage("Images/zon.png")
   myFont = loadFont('Fonts/StickNoBills-Regular.ttf');
@@ -17,34 +33,26 @@ function preload() {
   explosion = loadSound('Geluiden/boem2.mp3');
 }
 
-
-var g = 0.0;
-var c = 0.0;
-var P0, P1, P2, P3, P4;
-var startTime, endTime;
 var score = 0;
 var highscore = 0;
-var s = 0;
-var i = 0;
-var b = 100;
-var screen = 0;
-var apos = 510
+var screen = 1;
 var [xpos, zpos, xspeed, yspeed, gameover] = [450, 550, 0, 0, 0];
 
-
+var tegenLiggers = [];
 
 function draw() {
+
   //startscherm
   if (screen == 0) {
     menu()
   }
   //spel
-  else if (screen == 1) {
+  if (screen == 1) {
     game()
   }
   //wat er gebeurt bij het game-over scherm
-  else if (screen == 2) {
-    gameover()
+  if (screen == 2) {
+    gameOver()
   }
 }
 
@@ -92,8 +100,8 @@ function menu() {
 }
 
 //gameover scherm
-function gameover() {
-  music.stop()
+function gameOver() {
+  //music.stop()
   if (gameover == 0) {
     explosion.play();
     gameover = 1;
@@ -115,55 +123,26 @@ function gameover() {
 function game() {
   background(209, 173, 56);
 
-  b = b + 1;
-  // c = c + 0.0035;
-  // g = -sin(c) * 400;
-  c = c + 1;
-  // if (g <= -200){
-  //   g = g + 1;
-  // }
-
-  
-
   //maken driehoeken die de weg zijn en horizon
-  weg()
+  weg();
 
   //zon
   image(img9, 800, 40, 80, 80);
 
-
-  //tegenliggers
-
-  // move()
-  // show()
-  if (b % 250 === 0) {
-    spawn_tegenliggers();
+  if (frameCount % 250 === 0) {
+    tegenLiggers.push(new Tegenligger());
   }
-  P0 = createVector(550, 100);
-  P1 = createVector(apos - 60, 800);
-  P2 = createVector(apos + 500, 800);
-  P3 = createVector(apos - 600, 800);
-  P4 = createVector(apos + 1000, 800);
-  let currentTIme = millis();
-  let scale = min(1, (currentTIme - startTime) / (endTime - startTime));
-  // let scale = 5
-  let V_dist = p5.Vector.sub(P1, P0).mult(scale);
-  let PX = p5.Vector.add(P0, V_dist);
-  apos += xspeed;
-  image(img3, PX.x, PX.y, -c, -c);
+
+  tegenLiggers.forEach((t) => {
+    t.show();
+  });
 
 
-  //game over bij collision
-  if (apos <= 500 && apos >= 400 && b % 500 === 0) {
-    screen = 2;
-  }
-  
   //vrachtwagen interieur
   image(img1, 0, 0, 1000, 800);
 
   //score-systeem
-  s = s + 1;
-  if (s % 4 === 0) {
+  if (frameCount % 4 === 0) {
     score = score + 1
   }
   textSize(40);
@@ -181,29 +160,46 @@ function game() {
 
 //TEGENLIGGERS
 class Tegenligger {
-  constructor(img, x, y, w, h, target, vy) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.target = target
+  constructor(vy) {
     this.vy = vy;
+    this.c = 0;
+    this.lane = int(random(4)) + 1;
+    this.startTime = millis();
+    this.endTime = this.startTime + 5000;
+    this.apos = 510;
 
     this.p0 = createVector(550, 100);
-    this.p1 = createVector(apos - 60, 800);
+    console.log(this.lane, this.apos);
+    this.p1 = getLane(this.lane, this.apos);
   }
 
   move() {
 
   }
+
   show() {
-    let currentTime = millis();
-    let scale = min(1, (currentTime - startTime) / (endTime - startTime));
+
+    this.c++;
+
+
+    let scale = min(1, (millis() - this.startTime) / (this.endTime - this.startTime));
+
     // let scale = 5
-    let v_dist = p5.Vector.sub(P1, P0).mult(scale);
-    let px = p5.Vector.add(P0, v_dist);
-    apos += xspeed;
-    image(img3, px.x, px.y, -c, -c);
+    let v_dist = p5.Vector.sub(this.p1, this.p0).mult(scale);
+    let px = p5.Vector.add(this.p0, v_dist);
+    this.apos += xspeed;
+    this.p1 = getLane(this.lane, this.apos);
+    image(tegenliggerImg, px.x, px.y, -this.c, -this.c);
+
+    if (px.y > 600) {
+      let idx = tegenLiggers.indexOf(this);
+      tegenLiggers.splice(idx, 1);
+      //screen = 2;
+    }
+    //game over bij collision
+    // if (this.apos <= 500 && this.apos >= 400) {
+    //   screen = 2;
+    // }
   }
 }
 
@@ -238,11 +234,4 @@ function keyPressed() {
 
 function setLineDash(list) {
   drawingContext.setLineDash(list);
-}
-
-function spawn_tegenliggers() {
-  startTime = millis();
-  endTime = startTime + 5000;
-  c = 0;
-  g = 0;
 }
